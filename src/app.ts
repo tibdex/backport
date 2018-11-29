f4a64b0b8918d49684175442a02bdc9a79f6c6ae
Thibault Derousseaux <tibdex@gmail.com>
2018-11-28 20:59:19 -0500

    Migrate to Now 2.0

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { Application, Context } from "probot";
+
+import handleEvent from "./backport";
+
+const appFn = (app: Application) => {
+  app.log("App loaded");
+
+  app.on(
+    ["pull_request.labeled", "pull_request.closed"],
+    async (context: Context) => {
+      const { payload, github: octokit } = context;
+      const { number: pullRequestNumber, owner, repo } = context.issue();
+      await handleEvent({
+        // @ts-ignore The value is the good one even if the type doesn't match.
+        octokit,
+        owner,
+        payload,
+        pullRequestNumber,
+        repo,
+      });
+    },
+  );
+};
+
+export = appFn;
