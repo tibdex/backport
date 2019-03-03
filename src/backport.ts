498aa7b2f9ca654a21047116d74a7e60b48d3796
Matthias Kuhn <matthias@opengis.ch>
2019-03-04 00:31:37 +0100

    Allow creating multiple backports from one pull request
    Fix #17

--- src/backport.ts
+++ src/backport.ts
@@ -36,55 +36,57 @@ const backport = async ({
   pullRequestNumber: PullRequestNumber;
   repo: RepoName;
 }) => {
-  const labelName = payload.pull_request.labels
+  payload.pull_request.labels
     .map(({ name }) => name)
-    .find(name => regExp.test(name));
-  if (
-    !labelName ||
-    !payload.pull_request.merged ||
-    (payload.label && !regExp.test(payload.label.name))
-  ) {
-    // Ignore unlabeled or unmerged pull requests and unrelated label events.
-    return;
-  }
-  // Thanks to the code above, we're sure that labelName matches.
-  const [, base, head] = regExp.exec(labelName) as string[];
+    .filter(name => regExp.test(name))
+    .forEach(function(labelName) {
+    if (
+      !labelName ||
+      !payload.pull_request.merged ||
+      (payload.label && !regExp.test(payload.label.name))
+    ) {
+      // Ignore unlabeled or unmerged pull requests and unrelated label events.
+      return;
+    }
+    // Thanks to the code above, we're sure that labelName matches.
+    const [, base, head] = regExp.exec(labelName) as string[];
 
-  debug("backporting", {
-    base,
-    head,
-    owner,
-    pullRequestNumber,
-    repo,
-  });
-  try {
-    const backportedPullRequestNumber = await backportPullRequest({
+    debug("backporting", {
       base,
       head,
-      octokit,
       owner,
       pullRequestNumber,
       repo,
     });
-    debug("backported", backportedPullRequestNumber);
-    return backportedPullRequestNumber;
-  } catch (error) {
-    const message = "backport failed";
-    debug(message, error);
-    await octokit.issues.createComment({
-      body: [
-        `The backport to \`${base}\` failed:`,
-        "",
-        "```",
-        error.message,
-        "```",
-      ].join("\n"),
-      number: pullRequestNumber,
-      owner,
-      repo,
-    });
-    throw new Error(message);
-  }
+    try {
+      const backportedPullRequestNumber = await backportPullRequest({
+        base,
+        head,
+        octokit,
+        owner,
+        pullRequestNumber,
+        repo,
+      });
+      debug("backported", backportedPullRequestNumber);
+      return backportedPullRequestNumber;
+    } catch (error) {
+      const message = "backport failed";
+      debug(message, error);
+      await octokit.issues.createComment({
+        body: [
+          `The backport to \`${base}\` failed:`,
+          "",
+          "```",
+          error.message,
+          "```",
+        ].join("\n"),
+        number: pullRequestNumber,
+        owner,
+        repo,
+      });
+      throw new Error(message);
+    }
+  });
 };
 
 export { backport, Label };
